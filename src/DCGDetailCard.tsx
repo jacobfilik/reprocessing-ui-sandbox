2830530a84b1984b35e0955b43eeb919d0d23bda
Jacob Filik <jacob.filik@diamond.ac.uk>
2026-07-02 09:04:26 +0100

    add h5web viewer

--- src/DCGDetailCard.tsx
+++ src/DCGDetailCard.tsx
@@ -1,43 +1,60 @@
-import { Card, CardContent, Stack, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { getDCPage } from "./queryfunctions";
 import { useQuery } from "@tanstack/react-query";
 import DCDetailCard from "./DCDetailCard";
 
-export function DCGDetailCard(props: { dcgid: number }) {
+export function DCGDetailCard(props: {
+  dcgid: number;
+  setDcid: (id: number) => void;
+}) {
   const query = useQuery({
     queryKey: ["data-groups", "data-collections", props.dcgid, 10],
     queryFn: () => getDCPage(props.dcgid, 0, 10),
   });
   return (
     <Card>
-      <CardContent>
-        <Typography>
-          {query.data &&
-            query.data.items &&
-            query.data.items.length > 0 &&
-            query.data.items[0].fileTemplate}
-        </Typography>
-        <Stack spacing={"2px"}>
-          {query.data &&
-            query.data.items &&
-            query.data.items.map((dc, i) => {
-              return (
-                <Stack>
-                  <Typography>
-                    {dc.dataCollectionId}{" "}
-                    {dc.imageContainerSubPath
-                      ? dc.imageContainerSubPath
-                      : "No detector path"}
-                  </Typography>
-                  <DCDetailCard
-                    key={i}
-                    dcid={dc.dataCollectionId}
-                  ></DCDetailCard>
-                </Stack>
-              );
-            })}
-        </Stack>
-      </CardContent>
+      <CardActionArea
+        onClick={() => {
+          if (query.data && query.data.items && query.data.items.length > 0) {
+            props.setDcid(query.data.items[0].dataCollectionId);
+          }
+        }}
+      >
+        <CardContent>
+          <Typography>
+            {query.data &&
+              query.data.items &&
+              query.data.items.length > 0 &&
+              query.data.items[0].fileTemplate}
+          </Typography>
+          <Stack spacing={"2px"}>
+            {query.data &&
+              query.data.items &&
+              query.data.items.map((dc, i) => {
+                return (
+                  <Stack>
+                    <Typography>
+                      {dc.dataCollectionId}{" "}
+                      {dc.imageContainerSubPath
+                        ? dc.imageContainerSubPath
+                        : "No detector path"}
+                    </Typography>
+                    <DCDetailCard
+                      key={i}
+                      dcid={dc.dataCollectionId}
+                    ></DCDetailCard>
+                  </Stack>
+                );
+              })}
+          </Stack>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
